60940c626e6e1199b7eb7bdd13d027f00af9e473
Amir Hosein Fadaei <afadaei2@gmail.com>
2015-06-25 23:53:16 +0430

    Update Main.java
    added a console + encoding ability

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+package ir.amfad;
+
 /*Huffman code encoder
 *        Copyright (C) 2015  Amir Hosein Fadaei
 *
@@ -14,7 +16,7 @@
 *        You should have received a copy of the GNU General Public License
 *        along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
-import Tree.Node;
+import ir.amfad.Tree.Node;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
@@ -23,26 +25,47 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi! Please enter symbols:affluence like \"a:23\"");
-        System.out.println(": symbol is invalid");
-        System.out.println("only one character is available before :");
-        System.out.println("enter \"end\" to end!");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map map = new HashMap();
-        String in;
-        while(true){
-            try {
-                in = br.readLine();
-                if(in.equals("end"))
-                    break;
-                String[] parts = in.split(":");
-                map.put(parts[0],parts[1]);
-            } catch (Exception e) {
-                System.out.println("Error, Wrong Input... :|");
-                e.printStackTrace();
+        System.out.println("Hi! enter \":exit\" to exit!");
+        while(true) {
+            System.out.println("Notification : Started building tree ...");
+            System.out.println("Please enter symbols:affluence like \"a:23\"");
+            System.out.println(": symbol is invalid");
+            System.out.println("only one character is available before :");
+            System.out.println("enter \":end\" to end!");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            Map map = new HashMap();
+            String in;
+            while (true) {
+                try {
+                    in = br.readLine();
+                    if (in.equals(":end"))
+                        break;
+                    if (in.equals(":exit"))
+                        return;
+                    String[] parts = in.split(":");
+                    map.put(parts[0], parts[1]);
+                } catch (Exception e) {
+                    System.out.println("Error, Wrong Input... :|");
+                    e.printStackTrace();
+                }
+            }
+            Huffman h = new Huffman(map);
+            System.out.println("Ready for encoding!please enter sentences like \"Hello!\"");
+            System.out.println("Symbols must be defined in lang or they wont get encoded");
+            System.out.println("enter \":reset\" to reset the tree!");
+            while (true) {
+                try {
+                    in = br.readLine();
+                    if (in.equals(":end"))
+                        break;
+                    if (in.equals(":exit"))
+                        return;
+                    System.out.println("encoded sent is : " + h.encode(in));
+                } catch (Exception e) {
+                    System.out.println("Error, Wrong Input... :|");
+                    e.printStackTrace();
+                }
             }
         }
-        Node n = Huffman.build(map);
-        Huffman.compute(n);
     }
 }
